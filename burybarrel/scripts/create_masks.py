1c0fd489e79d93a0afae1f9ca7d3e2692c48522c
Jerry <jeyan@ucsd.edu>
2025-03-08 18:28:21 -0800

    Change mask selection to score based instead of stupid area ranking

--- burybarrel/scripts/create_masks.py
+++ burybarrel/scripts/create_masks.py
@@ -112,73 +112,77 @@ def _create_masks(imgdir, text_prompt, outdir, box_threshold=0.3, text_threshold
         )[0]
         boxes = results["boxes"]
         masks = results["masks"]
-        logits = results["scores"]
+        scores = results["scores"]  # bbox score from dino
+        mask_scores = results["mask_scores"]  # mask score from sam
+        if len(mask_scores.shape) == 0:
+            mask_scores = np.array([mask_scores])
 
         if len(masks) == 0:
             print(f"No objects of the '{text_prompt}' prompt detected in image {imgpath}")
-        else:
-            masks_np = [(mask * 255).astype(np.uint8) for mask in masks]
-            if closekernelsize > 0:
-                # kernel = np.ones((closekernelsize, closekernelsize))
-                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (closekernelsize, closekernelsize))
-                masks_np = [cv2.morphologyEx(mask_np, cv2.MORPH_CLOSE, kernel) for mask_np in masks_np]
-            if convexhull:
-                contours = [cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0] for mask in masks_np]
-                convexcontours = [cv2.convexHull(np.vstack(contour)) for contour in contours]
-                masks_np = [cv2.drawContours(np.zeros_like(masks_np[0]), [convexcontour], -1, 255, thickness=cv2.FILLED) for convexcontour in convexcontours]
+            continue
+        masks_np = [(mask * 255).astype(np.uint8) for mask in masks]
+        if closekernelsize > 0:
+            # kernel = np.ones((closekernelsize, closekernelsize))
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (closekernelsize, closekernelsize))
+            masks_np = [cv2.morphologyEx(mask_np, cv2.MORPH_CLOSE, kernel) for mask_np in masks_np]
+        if convexhull:
+            contours = [cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0] for mask in masks_np]
+            convexcontours = [cv2.convexHull(np.vstack(contour)) for contour in contours]
+            masks_np = [cv2.drawContours(np.zeros_like(masks_np[0]), [convexcontour], -1, 255, thickness=cv2.FILLED) for convexcontour in convexcontours]
 
-            bbox_mask_path = maskcomp_dir / f"{imgpath.stem}_img_with_mask.png"
-            bbox_mask_path.parent.mkdir(parents=True, exist_ok=True)
-            display_image_with_masks(image_pil, masks_np, boxes, logits, figwidth=13, savefig=bbox_mask_path, all_masks=True, show=False, show_confidence=True)
+        bbox_mask_path = maskcomp_dir / f"{imgpath.stem}_img_with_mask.png"
+        bbox_mask_path.parent.mkdir(parents=True, exist_ok=True)
+        display_image_with_masks(image_pil, masks_np, boxes, scores, mask_scores, figwidth=13, savefig=bbox_mask_path, all_masks=True, show=False, show_confidence=True)
 
-            # jank workaround for excluding those masks that are just supersets
-            # of the barrel itself
-            # reminder: [x_min, y_min, x_max, y_max]
-            # as it turns out jellyfish are detected for some reason, and they are smaller
-            # welp, just take the first one, it's usually the barrel anyway right?
-            rects = np.array([Polygon([(box[0], box[1]), (box[2], box[1]), (box[2], box[3]), (box[0], box[3])]) for box in boxes])
-            boxareas = np.array([rect.area for rect in rects])
-            maskareas = np.array([np.sum(mask_np > 0) for mask_np in masks_np])
-            validmask = np.ones(len(rects), dtype=bool)
-            for i in range(len(rects)):
-                if not validmask[i]:
-                    continue
-                if rects[i].area > 0.8 * w * h:
-                    # specifically for this data, objects should not cover more than 80% of the
-                    # image (in fact, it probably should not cover more than 20%)
-                    # this is specifically for those goofy bounding boxes that cover the whole
-                    # image for some reason
-                    validmask[i] = False
-                    continue
-                # for j in range(i+1, len(rects)):
-                #     # remove superset bounding boxes
-                #     if not validmask[j]:
-                #         continue
-                #     if rects[i].contains(rects[j]):
-                #         validmask[i] = False
-                #     elif rects[j].contains(rects[i]):
-                #         validmask[j] = False
-            # best_idx = np.argmin(boxareas)
-            if len(maskareas[validmask]) == 0:
-                print(f"No valid masks after filtering for {imgpath}")
-            else:
-                maxvalididx = np.argmax(maskareas[validmask])
-                best_idx = np.arange(len(rects))[validmask][maxvalididx]
+        # # jank workaround for excluding those masks that are just supersets
+        # # of the barrel itself
+        # # reminder: [x_min, y_min, x_max, y_max]
+        # # as it turns out jellyfish are detected for some reason, and they are smaller
+        # # welp, just take the first one, it's usually the barrel anyway right?
+        # rects = np.array([Polygon([(box[0], box[1]), (box[2], box[1]), (box[2], box[3]), (box[0], box[3])]) for box in boxes])
+        # boxareas = np.array([rect.area for rect in rects])
+        # maskareas = np.array([np.sum(mask_np > 0) for mask_np in masks_np])
+        # validmask = np.ones(len(rects), dtype=bool)
+        # for i in range(len(rects)):
+        #     if not validmask[i]:
+        #         continue
+        #     if rects[i].area > 0.8 * w * h:
+        #         # specifically for this data, objects should not cover more than 80% of the
+        #         # image (in fact, it probably should not cover more than 20%)
+        #         # this is specifically for those goofy bounding boxes that cover the whole
+        #         # image for some reason
+        #         validmask[i] = False
+        #         continue
+        #     # for j in range(i+1, len(rects)):
+        #     #     # remove superset bounding boxes
+        #     #     if not validmask[j]:
+        #     #         continue
+        #     #     if rects[i].contains(rects[j]):
+        #     #         validmask[i] = False
+        #     #     elif rects[j].contains(rects[i]):
+        #     #         validmask[j] = False
+        # # best_idx = np.argmin(boxareas)
+        # if len(maskareas[validmask]) == 0:
+        #     print(f"No valid masks after filtering for {imgpath}")
+        #     continue
+        # maxvalididx = np.argmax(maskareas[validmask])
+        # best_idx = np.arange(len(rects))[validmask][maxvalididx]
+        best_idx = np.argmax(scores)
 
-                # save masks
-                for i, mask_np in enumerate(masks_np):
-                    # each box is x_min, y_min, x_max, y_max
-                    bbox = boxes[i]
-                    mask_path = maskdebug_dir / f"{imgpath.stem}_mask_{i+1}.png"
-                    mask_image = Image.fromarray(mask_np)
-                    mask_image.save(mask_path)
+        # save masks
+        for i, mask_np in enumerate(masks_np):
+            # each box is x_min, y_min, x_max, y_max
+            bbox = boxes[i]
+            mask_path = maskdebug_dir / f"{imgpath.stem}_mask_{i+1}.png"
+            mask_image = Image.fromarray(mask_np)
+            mask_image.save(mask_path)
 
-                bbox = boxes[best_idx]
-                mask_np = masks_np[best_idx]
-                mask_path = outdir / f"{imgpath.stem}.png"
-                mask_image = Image.fromarray(mask_np)
-                mask_image.save(mask_path)
-                bboxes.append(bbox)
+        bbox = boxes[best_idx]
+        mask_np = masks_np[best_idx]
+        mask_path = outdir / f"{imgpath.stem}.png"
+        mask_image = Image.fromarray(mask_np)
+        mask_image.save(mask_path)
+        bboxes.append(bbox)
 
     bboxes = np.array(bboxes, dtype=int)
     with open(outdir / "bboxes.pickle", "wb") as f:
