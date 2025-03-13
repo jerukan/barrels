1454ea17fe9f7b62bcc1d54a229999a77deab23f
jerukan <jeyan@ucsd.edu>
2025-03-13 16:00:56 -0700

    Update keyframe script to match data folder format

--- burybarrel/scripts/get_footage_keyframes.py
+++ burybarrel/scripts/get_footage_keyframes.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import click
@@ -13,11 +14,11 @@ from burybarrel.image import apply_clahe
 
 @click.command()
 @click.option("-i", "--input", "input_path", required=True, type=click.Path(exists=True, dir_okay=False), help="input video path")
-@click.option("-o", "--output", "output_dir", required=False, type=click.Path(file_okay=False), help="output dir")
-@click.option("-t", "--time", "start_time", required=False, type=click.STRING, help="start time of the video")
-@click.option("--tz", "timezone", required=False, type=click.STRING, help="timezone")
+@click.option("-o", "--output", "output_dir", required=False, type=click.Path(file_okay=False), help="output dir (default: parent directory of video)")
+@click.option("-t", "--time", "start_time", required=False, type=click.STRING, help="start time of the video (ISO 8601 format) (YYYY-MM-DDTHH:MM:SS)")
+@click.option("--tz", "timezone", required=False, type=click.STRING, help="timezone from Olson tz database")
 @click.option(
-    "--fps", "fps", required=True, default=25, show_default=True, type=click.INT
+    "--fps", "fps", required=True, default=25, show_default=True, type=click.INT, help="Video FPS"
 )
 @click.option("--step", "step", required=True, type=click.INT, help="Number of frames between each image")
 @click.option(
@@ -63,23 +64,23 @@ def get_footage_keyframes(
         outdir = vidpath.parent / vidpath.stem
     else:
         outdir = Path(output_dir)
+    imgdir = outdir / "rgb"
     # top left x, y, width, height
     bbox = [0, 120, 1920, 875]
-    # nav depth data is extremely buggy, values just jump up for no reason
-    depth_noise_thresh = 6
 
-    outdir.mkdir(parents=True, exist_ok=True)
+    imgdir.mkdir(parents=True, exist_ok=True)
     vid = cv2.VideoCapture(str(vidpath))
     nframes = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
     fnames = []
 
     cnt = 0
-    for i in tqdm(range(0, nframes, step)):
+    for i in tqdm(range(0, nframes, step), desc="Saving images"):
         vid.set(cv2.CAP_PROP_POS_FRAMES, i)
         _, frame = vid.read()
         if frame is None:
             # video ended before expected number of frames
+            print("Video ended before expected number of frames")
             break
         if increase_contrast:
             frame = apply_clahe(frame, clipLimit=2.0, tileGridSize=(8, 8))
@@ -90,15 +91,21 @@ def get_footage_keyframes(
             cropped = frame
             fname = f"uncropped{str(cnt).zfill(4)}.png"
         fnames.append(fname)
-        cv2.imwrite(str(outdir / fname), cropped)
+        cv2.imwrite(str(imgdir / fname), cropped)
         cnt += 1
 
-    if start_time is None:
-        return
-    start_t = pd.Timestamp(start_time, tz=timezone)
-    dt = pd.Timedelta(seconds=step / fps)
-    curr_t = start_t
+    infodict = {
+        "object_name": "placeholder.ply"
+    }
+
     if navpath is not None:
+        # nav depth data is extremely buggy, values just jump up for no reason
+        depth_noise_thresh = 6
+        if start_time is None or timezone is None:
+            raise ValueError("start_time and timezone must be provided if navpath is provided")
+        start_t = pd.Timestamp(start_time, tz=timezone)
+        dt = pd.Timedelta(seconds=step / fps)
+        curr_t = start_t
         navpath = Path(navpath)
         nav = pd.read_csv(navpath)
         nav["timestamp"] = pd.to_datetime(nav["timestamp"])
@@ -146,3 +153,11 @@ def get_footage_keyframes(
             }
         )
         df.to_csv(outdir / "frame-time-nav.csv", index=False)
+        infodict = {
+            **infodict,
+            "lat": lats[0],
+            "lon": lons[0],
+            "depth": depths[0],
+        }
+    with open(outdir / "info.json", "wt") as f:
+        json.dump(infodict, f, indent=4)
